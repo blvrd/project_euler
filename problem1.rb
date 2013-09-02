279e7a5e026dbb06e04f790cd03dce6b9e955f4c
Garrett Martin <garrettqmartin8@optimum.net>
2013-09-01 20:17:03 -0400

    Problem 1 refactor

--- problem1.rb
+++ problem1.rb
@@ -1,9 +1,13 @@
 #Problem #1
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = (1..999).select {|num| num % 3 == 0 || num % 5 == 0}.reduce(:+)
+def multiples
+  sum = (1..999).select {|num| num % 3 == 0 || num % 5 == 0}.reduce(:+)
+  puts sum
+end
 
-puts sum
+multiples()
+
 
 #Answer is 233168
 
