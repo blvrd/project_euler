01389aa681bda74cbb1fc2729560dd59f5cef046
Garrett Martin <garrettqmartin8@optimum.net>
2013-09-02 19:19:11 -0400

    Problem 6 refactor

--- problem6.rb
+++ problem6.rb
@@ -1,28 +1,8 @@
 #Problem 6
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-
-def sum_of_squares
-  range = (1..100).to_a
-  new_arr = []
-
-  range.each do |x|
-    new_arr << (x * x)
-  end
-
-  sum = new_arr.reduce(:+)
-  
-end
-
-
-def square_of_sum
-  range = (1..100).to_a
-  sum = range.reduce(:+)
-  square = (sum * sum)
-end
-
 def difference
-  difference = (square_of_sum - sum_of_squares)
+  difference = (1..100).to_a.inject(:+) ** 2 - (1..100).to_a.map {|n| n ** 2}.inject(:+)
   puts difference
 end
 
