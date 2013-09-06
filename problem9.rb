bf59f9fcb79b4fcc03d0b9742e6e17fa72a760d0
Garrett Martin <garrettqmartin8@optimum.net>
2013-09-05 20:02:52 -0400

    Problem 9 solved but needs to be refactored

--- problem9.rb
+++ problem9.rb
@@ -1,11 +1,20 @@
 #Problem 9
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+require 'prime'
 
-#This is broken and awful.
-x = [3, 4 ,5]
-
-def pythagorean_triple?
-  puts true if (a**2 + b**2 == c**2)
+def create_triples
+  for a in (1..500)
+    for b in (1..500)
+      for c in (1..500)
+        if a*a + b*b == c*c
+          if a + b + c == 1000
+            puts (a*b*c)
+          end
+        end
+      end
+    end
+  end
 end
 
-x.pythagorean_triple?
+create_triples
+
