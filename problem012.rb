386220cedd953946faa124d515b8962f66173252
Garrett Martin <garrettqmartin8@optimum.net>
2013-09-10 21:59:56 -0400

    Problem 12 still wip

--- problem012.rb
+++ problem012.rb
@@ -1,8 +1,18 @@
 #Problem 12
 #What is the value of the first triangle number to have over five hundred divisors?
 
-def triangle_nums(n)
-  puts 1.upto(n).inject(:+)
+FACTOR_LIMIT = 500
+
+def find_factors(n)
+  factors = []
+  (1..n).each do |i|
+    factors.push(i) if n % i == 0
+    end
+  factors = factors.sort
+  return factors
 end
 
-triangle_nums(7)
+def triangle_nums(x)
+  1.upto(x).inject(:+)
+end
+
