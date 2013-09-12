4a07f1d58a785cce036518740bd22a7d8555f6fb
Garrett Martin <garrettqmartin8@optimum.net>
2013-09-12 16:24:01 -0400

    Problem 12 a mess but getting there

--- problem012.rb
+++ problem012.rb
@@ -4,15 +4,24 @@
 FACTOR_LIMIT = 500
 
 def find_factors(n)
-  factors = []
+  @factors = []
   (1..n).each do |i|
-    factors.push(i) if n % i == 0
+    @factors.push(i) if n % i == 0
     end
-  factors = factors.sort
-  return factors
+  @factors = @factors.sort
+  puts @factors
 end
 
-def triangle_nums(x)
-  1.upto(x).inject(:+)
+def triangle_num(x)
+  @num = ((x.to_f / 2) * (x + 1))
+  puts @num
 end
 
+def solution
+  x = 100
+  triangle_num(x)
+  puts find_factors(@num)
+end
+
+triangle_num(100)
+find_factors(@num)
